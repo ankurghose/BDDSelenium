82826d200df5bc0d97bee5150dd89835285e73f2
ankur <ankur@192.168.1.8>
2021-02-09 13:42:12 -0500

    "Pushing Pages "

---
+++ BDDFramework/src/test/java/pages/FormPages.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FormPages {
+	FormPages PG;
+	
+	public WebDriver driver;
+
+	public FormPages(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+	
+ @FindBy(xpath="//input[@id='firstName']")
+ public WebElement firstname;
+
+public WebElement getFirstname() {
+	return firstname;
+}
+ 
+ 
+ 
+	
+	
+	
+
+}
--- BDDFramework/src/test/java/stepDef/VailfFormPage.java
+++ BDDFramework/src/test/java/stepDef/VailfFormPage.java
@@ -1,21 +1,25 @@
 package stepDef;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
 import common.Base;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import pages.FormPages;
 
 public class VailfFormPage extends Base {
-
+FormPages PG;
 	
-	
+	@Test
 	@Given("^I am on the from's page$")
 	public void i_am_on_the_from_s_page()  {
 		getdriver();
 	    
+		PG = PageFactory.initElements(driver, FormPages.class);
 		
-		
-		
+		PG.firstname.sendKeys("Ankur");
 	}
 
 	@Given("^I fill out the page$")
