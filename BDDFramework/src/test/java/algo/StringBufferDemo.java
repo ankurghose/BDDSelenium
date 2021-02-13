7d90493ff059477d8e3c976fb66a011dab128e03
Syeda Mou <Syeda Mou@DESKTOP-FGP99V0.home>
2021-02-13 13:06:59 -0500

    StringBuffer class created

---
+++ BDDFramework/src/test/java/algo/StringBufferDemo.java
@@ -0,0 +1,27 @@
+package algo;
+
+public class StringBufferDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb = new StringBuffer(" Hello ");
+		   //sb.append("world");
+		   //System.out.println(sb);
+		   
+		  //sb.reverse();
+		  //System.out.println(sb);
+		   
+		   //sb.deleteCharAt(4);
+		  //System.out.println(sb);
+		   
+		   //sb.delete(4, 7);
+		   //System.out.println(sb);
+		   
+		   sb.replace(2, 3, "i");
+		   System.out.println(sb);
+		   
+		   
+		   
+	}
+
+}
