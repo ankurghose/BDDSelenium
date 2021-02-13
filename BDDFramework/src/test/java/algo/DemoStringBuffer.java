0988b460de503e82c0366992c821dbaf064a2639
ankur <ankur@192.168.1.3>
2021-02-13 13:08:11 -0500

    push

---
+++ BDDFramework/src/test/java/algo/DemoStringBuffer.java
@@ -0,0 +1,24 @@
+package algo;
+
+public class DemoStringBuffer {
+
+	public static void main(String[] args) {
+
+		
+	StringBuffer sb = new StringBuffer("hello ");
+	sb.append("world");
+	System.out.println(sb);
+	
+	
+	
+	sb.reverse();
+	System.out.println(sb);
+	
+	//sb.deleteCharAt(1);
+	//System.out.println(sb);
+	
+	
+		
+	}
+
+}
--- BDDFramework/src/test/java/algo/StringBufferDemo.java
+++ BDDFramework/src/test/java/algo/StringBufferDemo.java
@@ -1,6 +1,6 @@
 package algo;
 
-public class StringBuffer {
+public class StringBufferDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
